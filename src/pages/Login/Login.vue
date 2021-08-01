34c4e1f73e1b2e720b61d3ef38b68685e2f02e72
zhengxuechun <14740555956@163.com>
2021-08-01 21:59:57 +0800

    登录方式的切换已经做好

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -5,13 +5,13 @@
         <div class="login_header">
           <h2 class="login_logo">硅谷外卖</h2>
           <div class="login_header_title">
-            <a href="javascript:;" class="on">短信登录</a>
-            <a href="javascript:;">密码登录</a>
+            <a href="javascript:;" :class="{on:loginWay}" @click="loginWay=true">短信登录</a>
+            <a href="javascript:;" :class="{on:!loginWay}" @click="loginWay=false">密码登录</a>
           </div>
         </div>
         <div class="login_content">
           <form>
-            <div class="on">
+            <div :class="{on:loginWay}">
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机号">
                 <button disabled="disabled" class="get_verification">获取验证码</button>
@@ -24,7 +24,7 @@
                 <a href="javascript:;">《用户服务协议》</a>
               </section>
             </div>
-            <div>
+            <div :class="{on:!loginWay}">
               <section>
                 <section class="login_message">
                   <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
@@ -55,7 +55,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data(){
+    return{
+      loginWay:true, //true短信登录，false密码登录
+    }
+  }
+}
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>

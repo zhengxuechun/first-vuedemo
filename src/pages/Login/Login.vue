8da3c13afeae5397ba0247c9c5c1eca3646b8757
zhengxuechun <14740555956@163.com>
2021-08-02 21:42:31 +0800

    02结束 实现手机用户名登录转换功能

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -10,14 +10,14 @@
           </div>
         </div>
         <div class="login_content">
-          <form>
+          <form @submit.prevent="login">
             <div :class="{on:loginWay}">
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
                <button :disabled="!rightPhone" class="get_verification" :class="{right_phone:rightPhone}" @click.prevent="getCode">{{computeTime>0 ? `已发送(${computeTime}s)`:'获取验证码'}}</button>
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="验证码">
+                <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
               </section>
               <section class="login_hint">
                 温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
@@ -27,17 +27,18 @@
             <div :class="{on:!loginWay}">
               <section>
                 <section class="login_message">
-                  <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                  <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
                 </section>
                 <section class="login_verification">
-                  <input type="tel" maxlength="8" placeholder="密码">
-                  <div class="switch_button off">
-                    <div class="switch_circle"></div>
-                    <span class="switch_text">...</span>
+                  <input type="text" maxlength="8" placeholder="密码" v-if="showPwd" v-model="pwd">
+                  <input type="password" maxlength="8" placeholder="密码" v-else v-model="pwd">
+                   <div class="switch_button" :class="showPwd?'on':'off'" @click="showPwd=!showPwd">
+                    <div class="switch_circle" :class="{right:showPwd}"></div>
+                    <span class="switch_text">{{showPwd ? 'abc' : '...'}}</span>
                   </div>
                 </section>
                 <section class="login_message">
-                  <input type="text" maxlength="11" placeholder="验证码">
+                  <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
                   <img class="get_verification" src="./images/captcha.svg" alt="captcha">
                 </section>
               </section>
@@ -50,17 +51,26 @@
           <i class="iconfont icon-jiantou2"></i>
         </a>
       </div>
+      <AlertTip :alertText = "alertText" v-show="alertShow" @closeTip="closeTip"/>
     </section>
   </div>
 </template>
 
 <script>
+  import AlertTip from '../../components/AlertTip/AlertTip.vue'
 export default {
   data(){
     return{
       loginWay:true, //true短信登录，false密码登录
-      phone:'',
+      phone:'',//手机号
       computeTime:0,
+      showPwd:false,
+      pwd:'',//密码
+      name:'',//用户名
+      code:'',//短信验证码
+      captcha:'',//图形验证码
+      alertText:'',//提示文本
+      alertShow:false,//是否显示提示框
     }
   },
   computed:{
@@ -69,6 +79,7 @@ export default {
     }
   },
   methods: {
+    //异步获取短信验证码
     getCode () {
     if (!this.computeTime) {
       this.computeTime = 30
@@ -83,9 +94,86 @@ export default {
 
       //发送Ajax请求(向指定手机号发送验证码短信)
       
+    },
+  showAlert(alertText) {
+        this.alertShow = true
+        this.alertText = alertText
+      },
+      // 异步登陆
+      async login () {
+        let result
+        // 前台表单验证
+        if(this.loginWay) {  // 短信登陆
+          const {rightPhone, phone, code} = this
+          if(!this.rightPhone) {
+            // 手机号不正确
+            this.showAlert('手机号不正确')
+            return
+          } else if(!/^\d{6}$/.test(code)) {
+            // 验证必须是6位数字
+            this.showAlert('验证必须是6位数字')
+            return
+          }
+          // 发送ajax请求短信登陆
+          //result = await reqSmsLogin(phone, code)
+
+        } else {// 密码登陆
+          const {name, pwd, captcha} = this
+          if(!this.name) {
+            // 用户名必须指定
+            this.showAlert('用户名必须指定')
+            return
+          } else if(!this.pwd) {
+            // 密码必须指定
+            this.showAlert('密码必须指定')
+            return
+          } else if(!this.captcha) {
+            // 验证码必须指定
+            this.showAlert('验证码必须指定')
+            return
+          }
+          // 发送ajax请求密码登陆
+          //result = await reqPwdLogin({name, pwd, captcha})
+        }
+
+        // 停止计时
+        // if(this.computeTime) {
+        //   this.computeTime = 0
+        //   clearInterval(this.intervalId)
+        //   this.intervalId = undefined
+        // }
+
+        // 根据结果数据处理
+        // if(result.code===0) {
+        //   const user = result.data
+        //   // 将user保存到vuex的state
+        //   this.$store.dispatch('recordUser', user)
+        //   // 去个人中心界面
+        //   this.$router.replace('/profile')
+        // } else {
+        //   // 显示新的图片验证码
+        //   this.getCaptcha()
+        //   // 显示警告提示
+        //   const msg = result.msg
+        //   this.showAlert(msg)
+        // }
+      },
+      // 关闭警告框
+      closeTip () {
+        this.alertShow = false
+        this.alertText = ''
+      },
+      // 获取一个新的图片验证码
+      // getCaptcha () {
+      //   // 每次指定的src要不一样
+      //   this.$refs.captcha.src = 'http://localhost:4000/captcha?time='+Date.now()
+      // }
+    },
+
+    components: {
+      AlertTip
     }
-  },
-}
+  }
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
@@ -190,6 +278,8 @@ export default {
                     background #fff
                     box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                     transition transform .3s
+                    &.right
+                      transform translateX(30px)
               .login_hint
                 margin-top 12px
                 color #999
